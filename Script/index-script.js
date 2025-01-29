90b1322ab3dc1673ce9286c8f2cd252e3d7864c7
Christopher <christophcass@gmail.com>
2025-01-29 13:51:14 -0300

    HTML e CSS quase prontos

--- Script/index-script.js
+++ Script/index-script.js
@@ -1,15 +1,3 @@
-//6e496b0c0a1d24da18427ab07fb3e8bd
+//6e496b0c0a1d24da18427ab07fb3e8bd API key
 
 
-//Mostrar a temperatura, será alerado futuramente isso é apenas o teste
-var temperatura = document.getElementById('temperatura').innerHTML;
-var show_temperatura = document.getElementById('show_temperatura');
-
-if(temperatura>15){
-    let temperatura_alta = `<div class="calor">☼ ${temperatura}</div>`;
-    show_temperatura.innerHTML += temperatura_alta;
-} 
-else {
-    let temperatura_baixa = `<div class="frio">❆ ${temperatura}</div>`;
-    show_temperatura.innerHTML += temperatura_baixa;
-}
